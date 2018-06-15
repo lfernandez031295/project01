6fde82d92bc7fc6d479d78bf5e9908eb48faab51
jmpozzo <jmpozzo@dialdata.com.ar>
2018-06-15 20:47:52 -0300

    15-6-2018 porject01-d3

--- js/checkbox.js
+++ js/checkbox.js
@@ -4,7 +4,29 @@ function check_todos(vars){
 	
 	else $("input[type='checkbox']").attr("checked", false);
 
-	}
+};
+
+function valUlMay (){
+	var ulCuanti = $(".sucurales ul").length;
+	var anchoTotal = $(".ingrese").width();
+	var altoMayor = 0;
+	var altoActuaal =0;
+	for (var i = 0; i < ulCuanti; i++) {
+		console.log("valor de "+i);
+		altoActuaal = $(".sucurales ul:eq("+i+")").height();
+		if (altoActuaal > altoMayor) {
+			altoMayor = altoActuaal;
+		};
+		console.log(altoMayor);
+		var cadaAncho = (anchoTotal/4)-14;
+	};
+	$(".sucurales ul").css({"height" : altoMayor, "width": cadaAncho});
+	var altoBoton = $(".buscarCss").height();
+	$(".buscarCss").css("margin-top", altoMayor-altoBoton);
+
+};
+
+valUlMay();
 
 		
 		
